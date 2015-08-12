cde8a57cb19c92b07d539ac3b62d8df7c7831faf
panxiao <panxiao@cvte.com>
2015-08-12 17:38:21 +0800

    [add]新增botton组件

---
+++ src/js/components/button.js
@@ -0,0 +1,48 @@
+/**
+ * Created by panxiao on 15/8/12.
+ * Button elemnt
+ */
+var Asd = React.createClass({
+    propTypes:{
+        children: React.PropTypes.any,
+        className: React.PropTypes.string,
+        disabled: React.PropTypes.bool,
+        onClick: React.PropTypes.func,
+        style: React.PropTypes.object,
+        type: React.PropTypes.oneOf(['submit', 'button'])
+    },
+    getInitialState:function(){
+        return {
+            disabled:this.props.disabled,
+            className:'topcoat-button',
+            type: this.props.type ? this.props.type : 'button',
+            text: null
+        }
+    },
+    componentWillReceiveProps:function(newProps){
+        if (newProps.disabled !== this.props.disabled) {
+            this.setState({ disabled: newProps.disabled })
+        }
+    },
+    text:function(text){
+        this.setState({
+            text:text
+        });
+    },
+    handleClick() {
+        if (this.props.onClick) {
+            this.props.onClick()
+        }
+    },
+    render:function(){
+        return (
+            <button className={this.state.className}
+                    disabled={this.state.disabled}
+                    type={this.state.type}
+                    style={this.props.style}
+                    onClick={this.handleClick.bind(this)}>
+                {this.state.text || this.props.children}
+            </button>
+        );
+    }
+});
