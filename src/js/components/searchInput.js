1f3bad7d09c09bbc63773a93e43498a6e5ab327f
panxiao <panxiao@cvte.com>
2015-08-14 17:02:19 +0800

    [add]新增searchInput组件

---
+++ src/js/components/searchInput.js
@@ -0,0 +1,39 @@
+/**
+ * Created by panxiao on 15/8/13.
+ */
+var SearchButton = React.createClass({
+    inputTimeout:null,
+    propTypes:{
+        size: React.PropTypes.string,
+        onInput: React.PropTypes.func,
+        delay: React.PropTypes.number,
+        disabled: React.PropTypes.bool
+    },
+    getInitialState:function(){
+        return {
+            size: this.props.size ? this.props.size : '',
+            delay: this.props.delay ? this.props.delay : 300
+        }
+    },
+    handleInput: function(event){
+        var value = event.target.value;
+        if(this.inputTimeout){
+            window.clearTimeout(this.inputTimeout);
+        }
+        var that = this;
+        this.inputTimeout = setTimeout(function(){
+            that.props.onInput(value);
+        },this.state.delay);
+    },
+    render: function(){
+        var sizeClasss = this.state.size ? "topcoat-search-input--"+this.state.size :
+            "topcoat-search-input";
+        return (
+            <input type="search"
+                   placeholder='search'
+                   onInput={this.handleInput.bind(this)}
+                   className={sizeClasss}
+                />
+        );
+    }
+});
